671d323ba60666137cdf43d014fb55438ca92d27
Madalin2023 <madalin2023gmail.com>
2022-02-04 03:40:45 +0100

    Aggiunte operazioni di Somma Sottrazione Moltiplicazione e Divisione

---
+++ main.py
@@ -0,0 +1,42 @@
+from unittest import result
+
+
+print("===================================================")
+print("Benvenuto nella calcolatrice morente di Mada")
+print("===================================================")
+print(" ")
+
+continua=True
+while continua:
+
+    #Acquisizione dati
+    x=input("Primo numero: ")
+    segno=input("Operazione:[+,-,*,/] ")
+    y=input("Secondo numero: ")
+
+    #Verifica dati
+    if not(x.isdigit() and y.isdigit() and (segno=="+" or segno=="-" or segno=="*" or segno=="/")):
+        print("Dati errati")
+        continue
+
+    #Elaborazione dati
+
+    x=int(x)
+    y=int(y)
+    risultato=0
+    if segno=="+":
+        risultato=x+y
+    if segno=="-":
+        risultato=x-y
+    if segno=="*":
+        risultato=x*y
+    if segno=="/":
+        risultato=x/y
+
+
+    print("Il risultato è: ",risultato)
+    continua=(input("Vuoi continuare? [y]"))=="y"
+
+print("Chiusura in corso")
+
+
